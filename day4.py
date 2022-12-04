cb9a703e3a8cd58ea6551610d528345c8364c471
Leslie Hensley <leslie@badger-technologies.com>
2022-12-04 13:16:49 -0500

    Day 4 golf

--- day4.py
+++ day4.py
@@ -5,10 +5,8 @@ with open('data/day4.txt') as data:
         line = line.rstrip()
         low, high = sorted([[int(s) for s in sections.split('-')] for sections in line.split(',')],
                            key=lambda sec: [sec[0], -1 * sec[1]])
-        if low[0] <= high[0] and low[1] >= high[1]:
-            contained_count += 1
-        if low[1] >= high[0]:
-            overlap_count += 1
+        contained_count += low[0] <= high[0] and low[1] >= high[1]
+        overlap_count += low[1] >= high[0]
 
 print('Part 1')
 print(contained_count)
